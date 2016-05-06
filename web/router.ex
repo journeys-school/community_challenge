99cf373a2fe60f403c94bf2df671774a304b7076
Matthew Grady <matthewgrady@Matthew-Grady-MacBook-Air.local>
2016-05-06 11:59:32 -0600

    Added challenges controller

--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,8 @@ defmodule CommunityChallenge.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/challenges", ChallengeController, :index
+    get "/challenges/new", ChallengeController, :new
   end
 
   # Other scopes may use custom stacks.
---
+++ web/views/challenge_view.ex
@@ -0,0 +1,3 @@
+defmodule CommunityChallenge.ChallengeView do
+  use CommunityChallenge.Web, :view
+end
